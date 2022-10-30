62bae139c808c0417aebf145d088b04a8d4322c9
Darshan Patil <iampatildarsh>
2022-10-30 21:49:05 +0530

    Function java programs added

---
+++ src/com/bridgelabz/functional/Distance.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.functional;
+
+import java.util.*;
+import java.lang.Math;
+
+public class Distance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of x and y:");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        euclideanDistance(x,y);
+
+    }
+    static void euclideanDistance(int x, int y) {
+        System.out.println(Math.sqrt((x * x) + (y * y)));
+    }
+}
